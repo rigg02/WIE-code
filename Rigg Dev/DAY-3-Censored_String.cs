bbbaa3f8dccc6c226e0a4cf868b587345041923f
rigg02 <96586590+rigg02@users.noreply.github.com>
2022-04-28 15:06:49 +0530

    Censored String
    Using concept of string manipulation to uncensor a censored string.
    Question from
    https://edabit.com/challenge/wunaXvZw3WctYioeC

--- Rigg Dev/DAY-3-Censored_String.cs
+++ Rigg Dev/DAY-3-Censored_String.cs
@@ -4,7 +4,7 @@ string str = Console.ReadLine();
 Console.WriteLine("Enter the string of censored letters");
 string cor = Console.ReadLine();
 foreach (char ch in cor)
-{
+{ 
     int a = str.IndexOf('*');
     Console.WriteLine(a);
     str = str.Remove(a, 1).Insert(a, ch.ToString());
